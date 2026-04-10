f35f1a2082f2069b8d1e3282e3bec61d3c005c64
sogamradhika-bit <sogamradhika@gmail.com>
2026-04-10 20:33:32 +0530

    update

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ if not api_key:
 @st.cache_resource
 def load_model():
     genai.configure(api_key=api_key)
-    return genai.GenerativeModel("gemini-2.5-flash")
+    return genai.GenerativeModel("gemini-1.5-flash")
 
 model = load_model()
 
